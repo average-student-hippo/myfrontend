97a4fe4ed3a253e0972099c1a0726de3587ddcc0
average-student-hippo <kizzah.business@gmail.com>
2025-01-17 21:19:45 +0300

    change to live server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://africobackend.onrender.com/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://africobackend.onrender.com/";
